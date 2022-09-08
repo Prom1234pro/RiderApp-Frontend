eee967fb0932cde1b44f37fe582cc082409c1eac
victorosa <victorimadon@gmail.com>
2022-09-08 09:40:34 -0700

    initial commit

--- src/components/OtpVerificationCode.jsx
+++ src/components/OtpVerificationCode.jsx
@@ -1,23 +1,71 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState} from 'react'
 import Button from './Form/Button'
 function OtpCode(props) {
-  const timing = "00.30"
-  return (
-    <div className='OtpVerification-code'>
-      <div className="verification-num">
-        <input maxLength={1} type='text'/>
-        <input maxLength={1} type='text'/>
-        <input maxLength={1} type='text'/>
-        <input maxLength={1} type='text'/>
-      </div>
-				
-      <div className="otp-timing">
-        {timing}
-					<Button title="Create Verify"/>
-          <p>Did not receive OTP? <span>Send OTP</span></p>
-      </div>
-    </div>
-  )
+	const [otp, setOtp] = useState(new Array(4).fill(""));
+	const [filled, setFilled] = useState(false)
+	const [count, setCount] = useState(60);
+	useEffect(() => {
+		if (count <= 0 || filled) {
+			return
+	  }
+	  setTimeout(() => {
+	  setCount((count) => count -1);
+		
+	  }, 1000);
+	
+	  
+	}, [count, filled])
+	
+	useEffect(()=> {
+		const fill = otp.every((value)=> value !=="")
+		setFilled(fill)
+
+	}, [otp])
+	
+	const onOtpVerification = (element, index) =>{
+		// if(otp[index] !== "")element.nextSibling.focus();
+		// if(otp[index] === "")element.previousSibling.focus();
+		if(isNaN(element.value))
+		return false
+		
+		setOtp([...otp.map((d, idx) => (idx===index) ? element.value: d)]);
+		
+		if(element.value !==""){
+			element.nextSibling.focus();
+		}
+		if(element.value ===""){
+			element.previousSibling.focus()
+		}
+
+		
+	}
+
+	return (
+		<div className='OtpVerification-code'>
+		<div className="verification-num">
+		{otp.map( (data, index)=> {
+		return (
+			<input 
+			maxLength="1" 
+			type='text'
+			name="otp"
+			key={index}
+			value={data}
+			onInput={e =>onOtpVerification(e.target, index)}
+			onfocus={e => e.target.select()}
+			/>
+		);
+		})}
+		</div>
+					
+		<div className="otp-timing">
+			<p>{count}s</p>
+						<Button title="Create Verify"/>
+			<p>Did not receive OTP? <span>Send OTP</span></p>
+		</div>
+		</div>
+	)
 }
 
 export default OtpCode
